572977db92d4654ae3695190b346184680998801
Zach Harel <zach.harel42@gmail.com>
2025-06-21 22:10:20 -0400

    get rid of ktlint

--- road-runner/core/build.gradle.kts
+++ road-runner/core/build.gradle.kts
@@ -15,9 +15,6 @@ plugins {
 
     id("org.jetbrains.dokka")
 
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
-    id("org.jlleitschuh.gradle.ktlint-idea") version "10.2.1"
-
     `maven-publish`
 }
 
